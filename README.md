336d2c4d99606d2fac425c2a7880c8af423cc0bf
Kris Ploch <kris@ploch.dev>
2021-03-10 02:26:03 +0100

    More .net standard 2.0 changes

--- README.md
+++ README.md
@@ -4,10 +4,9 @@
 
 This repository is a collection of miscellaneous utility libraries.
 
-There're many similar libraries out there, but his one has a huge advantage: it's mine ;)
+There're similar libraries out there, but his one has a huge advantage: it's mine ;)
 
-Anyway, those utilities have proven to be very useful to me. I use them every day. 
-
+Anyway, those utilities have proven to be very useful to me. I use them every day.
 
 ## Projects and Packages
 
@@ -15,10 +14,13 @@ Repository consists of the following main projects:
 
 - Ploch.Common
 - Ploch.Data
-- Ploch.DependencyInjection
-- Ploch.DependencyInjection.Unity
+- Ploch.Common.Data.Model
 - Ploch.Common.FileSystem
-- Ploch.Common.
+- Ploch.DependencyInjection
+- Ploch.TestingSupport
+- Ploch.TestingSupport.FluentAssertions
+- Ploch.TestingSupport.UnityContainer
+- Ploch.TestingSupport.Xunit
 
 
 
--- src/Common/ObjectCloningHelpers.cs
+++ src/Common/ObjectCloningHelpers.cs
@@ -19,17 +19,17 @@ namespace Ploch.Common
             CopyPropertiesIncludeOnly(source, target, null);
         }
 
-        public static void CopyPropertiesIncludeOnly<T>(this T source, T target, params string[]? includedProperties)
+        public static void CopyPropertiesIncludeOnly<T>(this T source, T target, params string[] includedProperties)
         {
             CopyProperties(source, target, includedProperties, null);
         }
 
-        public static void CopyPropertiesExcluding<T>(this T source, T target, params string[]? excludedProperties)
+        public static void CopyPropertiesExcluding<T>(this T source, T target, params string[] excludedProperties)
         {
             CopyProperties(source, target, null, excludedProperties);
         }
 
-        private static void CopyProperties<T>([NotNull] this T source, [NotNull] T target, IEnumerable<string>? includedProperties, params string[]? excludedProperties)
+        private static void CopyProperties<T>([NotNull] this T source, [NotNull] T target, IEnumerable<string> includedProperties, params string[] excludedProperties)
         {
 
             var properties = typeof(T).GetProperties();
