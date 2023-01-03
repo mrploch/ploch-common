5fee30cb37414fbada29a7076ff3e81214c974e0
Kris Ploch <kris@ploch.dev>
2023-01-03 20:01:40 +0000

    Applying styles

--- src/Common.Data/Data.Model/Category.cs
+++ src/Common.Data/Data.Model/Category.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ploch.Common.Data.Model
 {
-    public class Category<TCategory> : IHasId<int>, 
+    public class Category<TCategory> : IHasId<int>,
                                        INamed,
                                        IHierarchicalWithParentComposite<TCategory>,
                                        IHierarchicalWithChildrenComposite<TCategory> where TCategory : IHierarchicalWithChildren<TCategory>, IHierarchicalWithParent<TCategory>
     {
+        [Key]
         public int Id { get; set; }
 
         public string? Name { get; set; }
