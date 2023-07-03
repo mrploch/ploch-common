9983334e4d20a3593370038bf5f7715536375243
Kris Ploch <kris@ploch.dev>
2023-07-03 17:07:37 +0100

    updated actions

--- src/Common.Tests/IO/PathUtilsTests.cs
+++ src/Common.Tests/IO/PathUtilsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Ploch.Common.IO;
 using Xunit;
 
@@ -10,7 +9,7 @@ namespace Ploch.Common.Tests.IO
         [Fact]
         public void GetDirectoryName_should_return_folder_name_in_provided_path()
         {
-            PathUtils.GetDirectoryName(@"c:\myrootfolder\mysubfolder\expected-folder-name").Should().Be("expected-folder-name");
+            PathUtils.GetDirectoryName(@"c:/myrootfolder/mysubfolder/expected-folder-name").Should().Be("expected-folder-name");
         }
     }
 }
