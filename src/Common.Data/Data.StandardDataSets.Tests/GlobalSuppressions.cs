f84283b2f8a8e5cc0ad1fc3e2a2fd12b444ef1bf
Kris Ploch <kris@ploch.dev>
2023-07-23 23:54:49 +0100

    Minor updates

---
+++ src/Common.Data/Data.StandardDataSets.Tests/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[module: SuppressMessage("ReSharper", "ExceptionNotDocumentedOptional", Justification = "Not important in the tests project."),
+         SuppressMessage("ReSharper", "ExceptionNotDocumented", Justification = "Not important in the tests project."),
+         SuppressMessage("ReSharper",
+                         "PossibleMultipleEnumeration",
+                         Justification = "Not important in the tests project and could reduce readability.",
+                         Scope = "namespaceanddescendants"),
+         SuppressMessage("ReSharper",
+                         "LambdaExpressionCanBeMadeStatic",
+                         Justification = "Optimizations like that are not important in the tests project.",
+                         Scope = "namespaceanddescendants"),
+         SuppressMessage("ReSharper",
+                         "LambdaExpressionCanBeMadeStatic",
+                         Justification = "Optimizations like that are not important in the tests project.",
+                         Scope = "namespaceanddescendants")]
