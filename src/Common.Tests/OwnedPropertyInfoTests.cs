c5a38e350ee3f14bee5414fd4a990a135711140e
Kris Ploch <kris@ploch.dev>
2023-12-10 02:01:44 +0000

    OwnedPropertyInfo

---
+++ src/Common.Tests/OwnedPropertyInfoTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using Objectivity.AutoFixture.XUnit2.AutoMoq.Attributes;
+using Ploch.Common.Tests.Reflection;
+using Xunit;
+
+namespace Ploch.Common.Linq;
+
+public class OwnedPropertyInfoTests
+{
+    [Theory]
+    [AutoMockData]
+    public void GetValue_should_return_property_value_for_source_object(TestTypes.MyTestClass obj)
+    {
+        var property = obj.GetProperty(o => o.StringProp2);
+
+        property.GetValue().Should().Be(obj.StringProp2);
+    }
+
+    [Theory]
+    [AutoMockData]
+    public void SetValue_should_set_property_value_on_source_object(TestTypes.MyTestClass obj)
+    {
+        var property = obj.GetProperty(o => o.StringProp2);
+
+        property.SetValue("new-value");
+
+        obj.StringProp2.Should().Be("new-value");
+    }
+}
