2e86712aa6477445aa5e1487d465b17dc9a3eb56
Kris Ploch <kris@ploch.dev>
2023-12-18 02:17:44 +0000

    Generic Repository integration testing cleanup

--- src/Common.Data/Data.GenericRepository/Data.GenericRepository.EFCore.IntegrationTesting/RepositoryServicesRegistrationHelper.cs
+++ src/Common.Data/Data.GenericRepository/Data.GenericRepository.EFCore.IntegrationTesting/RepositoryServicesRegistrationHelper.cs
@@ -6,20 +6,22 @@ namespace Ploch.Common.Data.GenericRepository.EFCore.IntegrationTesting;
 
 public static class RepositoryServicesRegistrationHelper
 {
-    public static (IServiceProvider, TDbContext) RegisterRepositoryServices<TDbContext>(IServiceCollection serviceCollection, string connectionString = "Filename=:memory:") where TDbContext : DbContext
+    public static (IServiceProvider, TDbContext) RegisterRepositoryServices<TDbContext>(IServiceCollection serviceCollection,
+                                                                                        string connectionString = "Filename=:memory:")
+        where TDbContext : DbContext
     {
         serviceCollection.AddDbContext<TDbContext>(builder =>
-                                                      {
-                                                          var connection = new SqliteConnection(connectionString);
-                                                          connection.Open();
-                                                          builder.UseSqlite(connection);
-                                                      });
+                                                   {
+                                                       var connection = new SqliteConnection(connectionString);
+                                                       connection.Open();
+                                                       builder.UseSqlite(connection);
+                                                   });
         serviceCollection.AddRepositories<TDbContext>();
-        
+
         var serviceProvider = serviceCollection.BuildServiceProvider();
         var testDbContext = serviceProvider.GetRequiredService<TDbContext>();
         testDbContext.Database.EnsureCreated();
-        
+
         return (serviceProvider, testDbContext);
     }
 }
