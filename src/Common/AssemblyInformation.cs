3566a8f4f7043c3403147be9113ca8f82eafa826
Kris Ploch <kris@ploch.dev>
2024-02-07 17:13:39 +0000

    AssemblyInformation tests

--- src/Common/AssemblyInformation.cs
+++ src/Common/AssemblyInformation.cs
@@ -4,7 +4,6 @@ namespace Ploch.Common;
 
 public class AssemblyInformation(string product, string description, string version)
 {
-    public static readonly AssemblyInformation Current = new(typeof(AssemblyInformation).Assembly);
 
     public AssemblyInformation(Assembly assembly) : this(assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product,
                                                          assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()!.Description,
