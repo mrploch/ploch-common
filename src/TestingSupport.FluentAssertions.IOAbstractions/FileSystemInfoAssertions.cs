629a3b33145a59819f9bb312a0491087e1377042
Krzysztof Ploch <kris@ploch.dev>
2025-06-05 02:03:16 +0200

    Trying to fix the tests

--- src/TestingSupport.FluentAssertions.IOAbstractions/FileSystemInfoAssertions.cs
+++ src/TestingSupport.FluentAssertions.IOAbstractions/FileSystemInfoAssertions.cs
@@ -4,41 +4,38 @@ using System.IO.Abstractions;
 using System.Linq;
 using FluentAssertions;
 using FluentAssertions.Collections;
-using FluentAssertions.Execution;
-using FluentAssertions.Primitives;
 
 namespace TestingSupport.FluentAssertions.IOAbstractions
 {
-    public class FileSystemInfoAssertions<TItems> : GenericCollectionAssertions<TItems> where TItems: IFileSystemInfo
+    public class FileSystemInfoAssertions<TItems> : GenericCollectionAssertions<TItems>
+        where TItems : IFileSystemInfo
     {
+        public FileSystemInfoAssertions(IEnumerable<TItems> actualValue) : base(actualValue)
+        { }
+
         protected override string Identifier => nameof(IFileSystemInfo);
 
-        public AndConstraint<FileSystemInfoAssertions<TItems>> HaveNamesEquivalentToIgnoringCase(params string[] fileSystemInfoNames)
-        {
-            return HaveNamesEquivalentToIgnoringCase(fileSystemInfoNames);
-        }
+        public AndConstraint<FileSystemInfoAssertions<TItems>> HaveNamesEquivalentToIgnoringCase(params string[] fileSystemInfoNames) =>
+            HaveNamesEquivalentToIgnoringCase(fileSystemInfoNames);
 
         public AndConstraint<FileSystemInfoAssertions<TItems>> HaveNamesEquivalentToIgnoringCase(IEnumerable<string> fileSystemInfoNames,
                                                                                                  string because = "",
-                                                                                                 params object[] becauseArgs)
-        {
-            return HaveNamesEquivalentTo(fileSystemInfoNames, StringComparer.OrdinalIgnoreCase, because, becauseArgs);
-        }
-        
+                                                                                                 params object[] becauseArgs) =>
+            HaveNamesEquivalentTo(fileSystemInfoNames, StringComparer.OrdinalIgnoreCase, because, becauseArgs);
+
         public AndConstraint<FileSystemInfoAssertions<TItems>> HaveNamesEquivalentTo(IEnumerable<string> fileSystemInfoNames,
                                                                                      StringComparer equalityComparer,
                                                                                      string because = "",
                                                                                      params object[] becauseArgs)
         {
-            Subject.Select(fsi => fsi.Name.TrimEnd('\\'))
+            Subject.Select(fsi => TrimEndPathSeparator(fsi.Name))
                    .OrderBy(name => name)
                    .Should()
-                   .BeEquivalentTo(fileSystemInfoNames.Select(name => name.TrimEnd('\\')).OrderBy(name => name), config => config.Using(equalityComparer));
+                   .BeEquivalentTo(fileSystemInfoNames.Select(TrimEndPathSeparator).OrderBy(static name => name), config => config.Using(equalityComparer));
+
             return new AndConstraint<FileSystemInfoAssertions<TItems>>(this);
         }
 
-        public FileSystemInfoAssertions(IEnumerable<TItems> actualValue) : base(actualValue)
-        {
-        }
+        private static string TrimEndPathSeparator(string path) => path.TrimEnd('\\').TrimEnd('/');
     }
 }
