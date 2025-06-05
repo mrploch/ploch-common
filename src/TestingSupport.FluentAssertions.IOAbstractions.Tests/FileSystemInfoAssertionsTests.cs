3c8ed74793412685b96f03b8d3704d13fc809978
Krzysztof Ploch <kris@ploch.dev>
2025-06-05 02:27:11 +0200

    Minor code cleanup

--- src/TestingSupport.FluentAssertions.IOAbstractions.Tests/FileSystemInfoAssertionsTests.cs
+++ src/TestingSupport.FluentAssertions.IOAbstractions.Tests/FileSystemInfoAssertionsTests.cs
@@ -2,52 +2,53 @@
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
-using System.Reflection;
-using FluentAssertions;
-using FluentAssertions.Equivalency;
-using FluentAssertions.Execution;
 using Xunit;
+using Xunit.Abstractions;
 
-namespace TestingSupport.FluentAssertions.IOAbstractions.Tests
+namespace TestingSupport.FluentAssertions.IOAbstractions.Tests;
+
+public class FileSystemInfoAssertionsTests(ITestOutputHelper output)
 {
-
-    public class FileSystemInfoAssertionsTests
+    [Fact]
+    public void HaveNamesEquivalentTo_should_compare_names_only_and_not_fail_if_the_match()
     {
-        private static IFileSystem BuildFileSystem()
-        {
-            var fileSystem = new MockFileSystem();
-            fileSystem.AddDirectory("c:\\folder1");
-            fileSystem.AddFile("c:\\folder1\\file1.txt", new MockFileData(Guid.NewGuid().ToString()));
-            fileSystem.AddFile("c:\\folder1\\file2.txt", new MockFileData(Guid.NewGuid().ToString()));
-            fileSystem.AddFile("c:\\folder1\\file3.txt", new MockFileData(Guid.NewGuid().ToString()));
-            fileSystem.AddDirectory("c:\\folder1\\folder1_1");
-            fileSystem.AddDirectory("c:\\folder1\\folder1_2");
-            return fileSystem;
-        }
+        var fileSystemInfoNames = new List<string> { "file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2" };
+        var fileSystemInfoNames2 = new List<string> { "file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2" };
 
-        [Fact]
-        public void HaveNamesEquivalentTo_should_compare_names_only_and_not_fail_if_the_match()
-        {
-            var fileSystemInfoNames = new List<string>() {"file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2"};
-            var fileSystemInfoNames2 = new List<string>() {"file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2"};
+        //   fileSystemInfoNames.Should().BeEquivalentTo(fileSystemInfoNames2, config => config.Using(StringComparer.Ordinal));
 
-         //   fileSystemInfoNames.Should().BeEquivalentTo(fileSystemInfoNames2, config => config.Using(StringComparer.Ordinal));
+        var fileSystem = BuildFileSystem();
 
-            var fileSystem = BuildFileSystem();
+        var fileSystemInfos = fileSystem.DirectoryInfo.New("c:/folder1").GetFileSystemInfos();
 
-            var fileSystemInfos = fileSystem.DirectoryInfo.New("c:\\folder1").GetFileSystemInfos();
+        fileSystemInfos.Should().HaveNamesEquivalentTo(fileSystemInfoNames, StringComparer.Ordinal);
+    }
 
-            fileSystemInfos.Should().HaveNamesEquivalentTo(fileSystemInfoNames, StringComparer.Ordinal);
-        }
+    [Fact]
+    public void HaveNamesEquivalentTo_should_compare_names_only_and_fail_if_they_dont_match_count_different()
+    {
+        var fileSystem = BuildFileSystem();
 
-        [Fact]
-        public void HaveNamesEquivalentTo_should_compare_names_only_and_fail_if_they_dont_match_count_different()
-        {
-            var fileSystem = BuildFileSystem();
+        var directories = fileSystem.Directory.GetDirectories("c:/");
+        output.WriteLine($"FileSystem PathSeparator: {fileSystem.Path.PathSeparator}");
+        output.WriteLine($"FileSystem DirectorySeparatorChar: {fileSystem.Path.DirectorySeparatorChar}");
+        output.WriteLine($"FileSystem DirectorySeparatorChar: {fileSystem.Path.VolumeSeparatorChar}");
+        var fileSystemInfos = fileSystem.DirectoryInfo.New("c:/folder1").GetFileSystemInfos();
 
-            var fileSystemInfos = fileSystem.DirectoryInfo.New("c:\\folder1").GetFileSystemInfos();
+        fileSystemInfos.Should().HaveNamesEquivalentTo(new[] { "file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2" }, StringComparer.Ordinal);
+    }
 
-            fileSystemInfos.Should().HaveNamesEquivalentTo(new[] {"file1.txt", "file2.txt", "file3.txt", "folder1_1", "folder1_2"}, StringComparer.Ordinal);
-        }
+    private static IFileSystem BuildFileSystem()
+    {
+        var fileSystem = new MockFileSystem();
+
+        fileSystem.AddDirectory("c:/folder1");
+        fileSystem.AddFile("c:/folder1/file1.txt", new MockFileData(Guid.NewGuid().ToString()));
+        fileSystem.AddFile("c:/folder1/file2.txt", new MockFileData(Guid.NewGuid().ToString()));
+        fileSystem.AddFile("c:/folder1/file3.txt", new MockFileData(Guid.NewGuid().ToString()));
+        fileSystem.AddDirectory("c:/folder1/folder1_1");
+        fileSystem.AddDirectory("c:/folder1/folder1_2");
+
+        return fileSystem;
     }
 }
