3cfdf9cf7fdd6c9f9129edf2e43da9ecfd162806
Krzysztof Ploch <kris@ploch.dev>
2025-06-14 15:03:26 +0200

    feat(app-services): #157 Ploch.Common.AppServices Library
    Ploch.Common.AppServices library providing interfaces for common application services,
    that can be implemented by various providers.

---
+++ src/Common.AppServices/README.md
@@ -0,0 +1,5 @@
+# Ploch.Common.AppServices
+
+## Overview
+
+Provides abstractions for common services that can be implemented by different providers.
---
+++ src/Common.AppServices/Security/IUserInfoProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace Ploch.Common.AppServices.Security;
+
+/// <summary>
+///     Provides information about the current user.
+/// </summary>
+public interface IUserInfoProvider
+{
+    /// <summary>
+    ///     Returns the current user.
+    /// </summary>
+    /// <returns>Current user.</returns>
+    ClaimsPrincipal? GetCurrentUserInfo();
+}
